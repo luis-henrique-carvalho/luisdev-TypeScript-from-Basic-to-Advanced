e595e2c21f7a454140e04f48c99dd5eeb1a81ba1
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-05-17 15:24:42 -0300

    :sparckles: feat: type number

---
+++ 2_FUNDAMENTALS/index.js
@@ -0,0 +1,8 @@
+// 1 - Number
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.5546456;
+console.log(typeof y);
+console.log(y);
---
+++ 2_FUNDAMENTALS/index.ts
@@ -0,0 +1,19 @@
+// 1 - Number
+let x: number = 10
+
+console.log(x)
+
+x = 16
+
+console.log(typeof x)
+
+
+let y: number = 15.5546456
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toPrecision(3))
+
+
