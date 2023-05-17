bc9b0b1de1e3b80d899968d9dc3fcc0ae1aadee1
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-05-17 15:32:05 -0300

    :sparkles: feat: type string

--- 2_FUNDAMENTALS/index.js
+++ 2_FUNDAMENTALS/index.js
@@ -6,3 +6,12 @@ console.log(typeof x);
 var y = 15.5546456;
 console.log(typeof y);
 console.log(y);
+console.log(y.toPrecision(3));
+// 1 - String
+var firstName = "Matheus";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Battisti";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof lastName);
--- 2_FUNDAMENTALS/index.ts
+++ 2_FUNDAMENTALS/index.ts
@@ -1,19 +1,32 @@
 // 1 - Number
-let x: number = 10
+let x: number = 10;
 
-console.log(x)
+console.log(x);
 
-x = 16
+x = 16;
 
-console.log(typeof x)
+console.log(typeof x);
 
+let y: number = 15.5546456;
 
-let y: number = 15.5546456
+console.log(typeof y);
 
-console.log(typeof y)
+console.log(y);
 
-console.log(y)
+console.log(y.toPrecision(3));
 
-console.log(y.toPrecision(3))
+// 2 - String
 
+const firstName: string = "Matheus";
 
+console.log(firstName.toUpperCase());
+
+let fullName: string;
+
+const lastName: string = "Battisti";
+
+fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+console.log(typeof lastName);
